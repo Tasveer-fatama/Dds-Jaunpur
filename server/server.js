ada8eb94ac8d93e40a53cfc77de4adcc6bb8fb87
tasveer-fatama <tasveerfatema135@gmail.com>
2025-08-19 23:19:55 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+const app = express();
+
+app.get("/", (req, res) =>
+     res.send("DDS Institution API is running")
+);
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`);
+});
